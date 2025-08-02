789d57a44e4bccadf265459ceca342809d823390
AapoK <aapo.e.kojo@gmail.com>
2025-08-03 00:19:07 +0300

    Fixing stuff for japping meister

--- Assets/RotateTowardsPlayer.cs
+++ Assets/RotateTowardsPlayer.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class RotateTowardsPlayer : MonoBehaviour
 {
+    Transform player => GameObject.FindGameObjectWithTag("Player").transform;
     void LateUpdate()
     {
         Rotate();
@@ -9,5 +10,6 @@ public class RotateTowardsPlayer : MonoBehaviour
     void Rotate()
     {
         transform.forward = Camera.main.transform.forward;
+        //transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
     }
 }
